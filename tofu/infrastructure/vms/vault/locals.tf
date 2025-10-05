7b9537c16ae70fcf55d21b1c825fd4085e81f873
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2025-10-05 12:29:03 +0200

    remove vault vm

--- tofu/infrastructure/vms/vault/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  vm_name       = "vault"
-  vault_version = "1.19.5"
-  mount_path    = "/mnt/data"
-}
--- tofu/infrastructure/vms/vault/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "proxmox_node_name" {
-  type        = string
-  description = "The name of the Proxmox node to deploy the Vault VM on."
-}
