b3bb11cd3b3b1d5bd71462e6cb42b077a1b10796
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2025-10-06 21:21:05 +0200

    add output params

--- tofu/infrastructure/vms/modules/data_disk_vm/outputs.tf
+++ tofu/infrastructure/vms/modules/data_disk_vm/outputs.tf
@@ -7,3 +7,15 @@ output "name" {
   description = "Name of the data disk VM"
   value       = proxmox_virtual_environment_vm.data_disk_vm.name
 }
+
+output "datastore_id" {
+  value = local.datastore_id
+}
+
+output "file_format" {
+  value = local.file_format
+}
+
+output "path_in_datastore" {
+  value = try(proxmox_virtual_environment_vm.data_disk_vm.disk[0].path_in_datastore, null)
+}
