c8ed4b728685ab286c48a8e5f7c4fb07b2f39b10
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2025-10-06 21:20:52 +0200

    update to locals

--- tofu/infrastructure/vms/modules/data_disk_vm/main.tf
+++ tofu/infrastructure/vms/modules/data_disk_vm/main.tf
@@ -1,3 +1,7 @@
+locals {
+  datastore_id = "zfs-longhorn"
+  file_format  = "raw"
+}
 resource "proxmox_virtual_environment_vm" "data_disk_vm" {
   name      = "${var.consumer_vm_name}-data-vm"
   node_name = var.node_name
@@ -6,9 +10,10 @@ resource "proxmox_virtual_environment_vm" "data_disk_vm" {
   tags      = concat(["ubuntu", "data-vm", "do-not-start"], var.additional_tags)
 
   disk {
-    datastore_id = "zfs-longhorn"
+    datastore_id = local.datastore_id
     interface    = "scsi0"
     size         = var.size
+    file_format  = local.file_format
   }
 
   lifecycle {
