0f41e50d0f0bf658ae381fcc38dc9b307d383511
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2025-11-01 10:05:00 +0100

    feat: adding disks for testing

---
+++ tofu/infrastructure/vms/sandbox/data_vm.tf
@@ -0,0 +1,29 @@
+resource "proxmox_virtual_environment_vm" "this" {
+  node_name = var.proxmox_node_name
+  started   = false
+  on_boot   = false
+  name      = "${local.vm_name}-data-vm"
+  tags      = ["ubuntu", "data-vm", "dev", "do-not-start"]
+
+
+  disk {
+    datastore_id = "zfs-longhorn"
+    interface    = "scsi0"
+    size         = 1
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "ubuntu" {
+  node_name = var.proxmox_node_name
+  started   = false
+  on_boot   = false
+  name      = "${local.vm_name}-data-vm"
+  tags      = ["data-vm", "dev", "do-not-start"]
+
+
+  disk {
+    datastore_id = "zfs-longhorn"
+    interface    = "scsi0"
+    size         = 1
+  }
+}
