7f050f1cf79b4dec92b360342e90b071a12ee173
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2025-11-01 10:01:37 +0100

    fix: remove obselete local

--- tofu/infrastructure/vms/artifactory/data_vm.tf
+++ tofu/infrastructure/vms/artifactory/data_vm.tf
@@ -1,10 +1,6 @@
-locals {
-  data_disk_size = 100
-}
-
 module "data_vm" {
   source           = "../modules/data_disk_vm"
   consumer_vm_name = local.vm_name
   node_name        = var.proxmox_node_name
-  size             = local.data_disk_size
+  size             = 100
 }
