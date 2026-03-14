97ccd18f4db8327c99ab94a2e7df72abe2cf7475
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2026-03-14 10:07:42 +0100

    fix: host variable

--- tofu/services/vault/data/get_k8s_info.sh
+++ tofu/services/vault/data/get_k8s_info.sh
@@ -1,5 +1,5 @@
 TOKEN=$(kubectl get secret -n vault -o jsonpath='{.items[0].data.token}' | base64 -d)
-HOST=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')
+HOST="$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}')"
 CA_CRT=$(kubectl get cm kube-root-ca.crt -o jsonpath="{['data']['ca\.crt']}")
 
 jq -n \
