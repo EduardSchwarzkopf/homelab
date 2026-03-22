3289e24721fa0019f73edf9588112938ff344bba
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2026-03-22 13:03:11 +0100

    chore: move to versions.tf

--- tofu/infrastructure/provider.tf
+++ tofu/infrastructure/provider.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.84.1"
-    }
-    ssh = {
-      source  = "loafoe/ssh"
-      version = "2.7.0"
-    }
-    pihole = {
-      source  = "ryanwholey/pihole"
-      version = "2.0.0-beta.1"
-    }
-    postgresql = {
-      source  = "cyrilgdn/postgresql"
-      version = "1.25.0"
-    }
-    vault = {
-      source  = "opentofu/vault"
-      version = "4.4.0"
-    }
-  }
-}
-
 provider "proxmox" {
   endpoint  = "https://proxmox.lan.schwarzkopf.center"
   api_token = "${var.proxmox_api_token_id}=${var.proxmox_api_token_secret}"
@@ -40,6 +15,4 @@ provider "pihole" {
 }
 
 # Vault provider will read VAULT_ADDR and VAULT_TOKEN from env by default.
-provider "vault" {
-  address = "https://vault.lan.schwarzkopf.center/"
-}
+provider "vault" {}
---
+++ tofu/infrastructure/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.84.1"
+    }
+    ssh = {
+      source  = "loafoe/ssh"
+      version = "2.7.0"
+    }
+    pihole = {
+      source  = "ryanwholey/pihole"
+      version = "2.0.0-beta.1"
+    }
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "1.25.0"
+    }
+    vault = {
+      source  = "opentofu/vault"
+      version = "4.4.0"
+    }
+  }
+}
