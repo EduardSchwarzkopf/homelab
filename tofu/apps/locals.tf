761b92f8c26c8b66a527323aa232570762ec3073
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2026-04-03 20:26:33 +0200

    feat: remove secret

--- tofu/apps/locals.tf
+++ tofu/apps/locals.tf
@@ -5,5 +5,4 @@ locals {
   lan_domain         = "${local.local_tld}.schwarzkopf.center"
   local_domain       = "home.${local.local_tld}"
   postgres_host      = "database-pg-prod.${local.local_tld}"
-  ssh_authorized_key = trimspace(file("${path.module}/data/authorized_key.pub"))
 }
