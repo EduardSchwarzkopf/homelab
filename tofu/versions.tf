adec4769856102717f1beea23a9e39dfbc75be73
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2026-06-07 17:54:34 +0200

    chore: bump proxmox version

--- tofu/versions.tf
+++ tofu/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.84.1"
+      version = "0.109.0"
     }
     ssh = {
       source  = "loafoe/ssh"
